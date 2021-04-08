62d6433bf6a6a91157e56df5395fcf413be63fa9
Aloisio Costa <aloisio.costa@miniclip.com>
2021-04-08 12:06:09 +0100

    PWA Dev

--- sw.js
+++ sw.js
@@ -2,4 +2,7 @@
 self.addEventListener('install', function(event) {
     // Perform install steps
     console.log('Installing...');
+    event.waitUntil(
+        console.log('Installed')
+    );
 });
